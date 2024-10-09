fe0f3b0a4ac1fa5d43a9b97481734971c1b39671
Uoleran <uoleran@mail.ru>
2024-10-09 19:16:07 +0300

    Загитил функцию

--- first.py
+++ first.py
@@ -2,3 +2,9 @@ print('Hello freak bitches')
 print('Hello freeeeee')
 print('Моя ты панда, моя ты пропаганда')
 
+def fun (one: int, two: str = 'Hello'):
+    one = one
+    two = two
+    print (two[:one])
+
+fun(3)
